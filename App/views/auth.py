abb9437b2509f5ba1e3e3d7975c9fb3c3cf99719
vernados123 <vernados123@gmail.com>
2025-04-20 18:58:40 -0400

    feat(routes): [#6] Added, Refactored, and Organized views and routes.
    - Added marker_views and CRUD routes.
    - Refactored existing views; removed unnecessary routes
    - Added routes for admin edit page and user map viewing (index/home page)

--- App/views/auth.py
+++ App/views/auth.py
@@ -1,32 +1,8 @@
-from flask import Blueprint, render_template, jsonify, request, flash, send_from_directory, flash, redirect, url_for
+from flask import Blueprint, request, redirect, url_for, flash, jsonify
 from flask_jwt_extended import jwt_required, current_user, unset_jwt_cookies, set_access_cookies
-
-
-from.index import index_views
-
-from App.controllers import (
-    login,
-    get_all_users
-)
+from App.controllers import login
 
 auth_views = Blueprint('auth_views', __name__, template_folder='../templates')
-
-
-
-
-'''
-Page/Action Routes
-'''    
-@auth_views.route('/users', methods=['GET'])
-def get_user_page():
-    users = get_all_users()
-    return render_template('users.html', users=users)
-
-@auth_views.route('/identify', methods=['GET'])
-@jwt_required()
-def identify_page():
-    return render_template('message.html', title="Identify", message=f"You are logged in as {current_user.id} - {current_user.username}")
-    
 
 @auth_views.route('/login', methods=['POST'])
 def login_action():
@@ -34,40 +10,20 @@ def login_action():
     token = login(data['username'], data['password'])
     response = redirect(request.referrer)
     if not token:
-        flash('Bad username or password given'), 401
+        flash('Bad username or password given')
     else:
         flash('Login Successful')
-        set_access_cookies(response, token) 
+        set_access_cookies(response, token)
     return response
 
 @auth_views.route('/logout', methods=['GET'])
 def logout_action():
-    response = redirect(request.referrer) 
+    response = redirect(url_for('main_views.index_page'))
     flash("Logged Out!")
     unset_jwt_cookies(response)
     return response
 
-'''
-API Routes
-'''
-
-@auth_views.route('/api/login', methods=['POST'])
-def user_login_api():
-  data = request.json
-  token = login(data['username'], data['password'])
-  if not token:
-    return jsonify(message='bad username or password given'), 401
-  response = jsonify(access_token=token) 
-  set_access_cookies(response, token)
-  return response
-
-@auth_views.route('/api/identify', methods=['GET'])
+@auth_views.route('/identify', methods=['GET'])
 @jwt_required()
-def identify_user():
-    return jsonify({'message': f"username: {current_user.username}, id : {current_user.id}"})
-
-@auth_views.route('/api/logout', methods=['GET'])
-def logout_api():
-    response = jsonify(message="Logged Out!")
-    unset_jwt_cookies(response)
-    return response
+def identify_page():
+    return jsonify({'message': f"Logged in as {current_user.username}"})
--- App/views/index.py
+++ App/views/index.py
@@ -1,14 +1,28 @@
-from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
-from App.controllers import (create_user,
-                             initialize,
-                             get_all_campuses,
-                             get_campus,
-                             get_all_categories,
-                             get_all_markers_for_campus_json,
-                             get_all_markers_filtered_json)
+from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify, flash
+from flask_jwt_extended import jwt_required, current_user
+from App.controllers import (
+        initialize,
+        get_all_campuses,
+        get_campus,
+        get_all_categories,
+        get_all_markers_for_campus_json,
+        get_all_markers_filtered_json
+        )
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
+
+@index_views.route('/init', methods=['GET'])
+def init():
+    initialize()
+    return jsonify(message='db initialized!')
+
+@index_views.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({'status':'healthy'})
+
+
+# Home Page
 @index_views.route('/', methods=['GET'])
 @index_views.route('/<campus_id>', methods=['GET'])
 def index_page(campus_id=1):
@@ -27,18 +41,10 @@ def index_page(campus_id=1):
             marker for marker in markers if search_query in marker['name'].lower() or search_query in marker['description'].lower()
         ]
         
-    return render_template('index.html', 
+    return render_template('index.html',
+                           user=current_user,
                            campuses=get_all_campuses(), 
                            selected_campus=get_campus(campus_id),
                            categories=get_all_categories(),
                            markers=markers,
                            search_query=search_query)
-
-@index_views.route('/init', methods=['GET'])
-def init():
-    initialize()
-    return jsonify(message='db initialized!')
-
-@index_views.route('/health', methods=['GET'])
-def health_check():
-    return jsonify({'status':'healthy'})
