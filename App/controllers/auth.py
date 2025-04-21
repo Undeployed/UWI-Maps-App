cb000b0e0a915210fedf35116c5077538de0853f
Imjustmw <jonathan.joseph2@my.uwi.edu>
2025-04-20 21:11:13 -0400

    fix(authentication): [#4] Fixed Authentication Error
    - Disabled user identity lookup
    - Modified controllers/auth.py to reflect changes

--- App/controllers/auth.py
+++ App/controllers/auth.py
@@ -6,7 +6,7 @@ from App.models import User
 def login(username, password):
   user = User.query.filter_by(username=username).first()
   if user and user.check_password(password):
-    return create_access_token(identity=username)
+    return create_access_token(identity=str(user.id))
   return None
 
 
@@ -14,12 +14,12 @@ def setup_jwt(app):
   jwt = JWTManager(app)
 
   # configure's flask jwt to resolve get_current_identity() to the corresponding user's ID
-  @jwt.user_identity_loader
-  def user_identity_lookup(identity):
-    user = User.query.filter_by(username=identity).one_or_none()
-    if user:
-        return user.id
-    return None
+  #@jwt.user_identity_loader
+  #def user_identity_lookup(identity):
+  #  user = User.query.filter_by(username=identity).one_or_none()
+  #  if user:
+  #      return user.id
+  #  return None
 
   @jwt.user_lookup_loader
   def user_lookup_callback(_jwt_header, jwt_data):
